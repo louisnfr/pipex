c74bbf52bc9ef7ac0964a5717d4162575ee386ee
Louis RAFFIN <lraffin@e3r4p8.42.fr>
2021-09-15 16:29:08 +0200

    push_swap

---
+++ inc/pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/15 16:05:40 by lraffin           #+#    #+#             */
+/*   Updated: 2021/09/15 16:28:53 by lraffin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdio.h>
+
+#endif
