57ad7dcd8ce64eab80b47fde9e29206c979cd267
Louis RAFFIN <lraffin@e3r4p8.42.fr>
2021-09-16 18:20:06 +0200

    push_swap

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 17:18:22 by lraffin           #+#    #+#             */
-/*   Updated: 2021/09/15 19:25:46 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/09/16 18:19:45 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,9 @@ open_pipes()
 	while (++i < 2 + 1)
 		pipe(end[i]);
 }
-pipex()
+void	pipex(int f1, int f2, char **av, char **envp)
 {
 	char **cmd;
-	int infile;
-	int outfile;
 	int pid;
 	int i = -1;
 
@@ -36,22 +34,20 @@ pipex()
 		pid = fork();
 		if (pid == 0)
 		{
-			infile = open(argv[1], O_RDONLY, 0644);
-			dup2(infile, STDIN_FILENO);
+			dup2(f1, STDIN_FILENO);
 			close(end[i][0]);
 			dup2(end[i][1], STDOUT_FILENO);
 			close(end[i][1]);
 			get paths
-			cmd = ft_split(argv[2], ' ');
+			cmd = ft_split(av[2], ' ');
 			execve;
 		}
 	}
 	parent_process(
 		i = -1;
-		outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		dup2(pipe[1][0], STDIN_FILENO);
 		close(pipe[1][0]);
-		dup2(outfile, STDOUT_FILENO);
+		dup2(f2, STDOUT_FILENO);
 		while (++i < 2)
 			wait(0);
 	)
@@ -59,10 +55,16 @@ pipex()
 
 int	main(int ac, char **av, char **envp)
 {
-	if (ac == 4)
-	{
-		pipex();
-	}
+	int f1;
+	int f2;
+
+	if (ac != 4)
+		return (-1);
+	f1 = open(av[1], O_RDONLY);
+	f2 = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (f1 < 0 || f2 < 0)
+		return (-1);
+	pipex(f1, f2, av, envp);
 	free_all();
 	return (0);
 }
